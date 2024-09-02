adb5ca3f863f066a18a020370d2321ff92f9b663
Dov Azogui <dov.azogui@gmail.com>
2024-09-01 23:04:55 -0400

    aaa

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  const createtask = document.getElementById('create-task-form')
+  createtask.addEventListener('submit',handleSubmit)
+
+
 });
+
+function handleSubmit(e){
+  e.preventDefault()
+  createelement(e.target['new-task-description'].value);
+
+}
+
+function createelement(event){
+  let li = document.createElement('li')
+  let tasks = document.getElementById('tasks')
+  li.textContent = `${event} `
+  btn = document.createElement('button')
+  //btn.addEventListener('click', (e) => e.target.parentNode.remove()) //btn.parentNode.remove())
+  btn.addEventListener('click', () => tasks.removeChild(li))
+  btn.textContent = 'x'
+  li.appendChild(btn)
+  tasks.appendChild(li)
+
+}
+
+// function hundledelete(){
+
+// }
+
+
+
+
+// const text = document.getElementById("new-task-description")
+// const tasks = document.getElementById('tasks')
+// const textcnt = text.textContent 
+// text.appendChild(textcnt)
+
+// console.log(text)
